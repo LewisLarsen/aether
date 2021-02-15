c11b24f8b6984c8cbb3b631bedd2fef970e900c8
Lewis Larsen <LewisLarsen@users.noreply.github.com>
2021-02-15 09:28:29 +0000

    Apply fixes from StyleCI

--- stubs/App/Http/Controllers/Account/UpdateEmailController.php
+++ stubs/App/Http/Controllers/Account/UpdateEmailController.php
@@ -3,31 +3,32 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
+use App\Models\User;
 use Auth;
 use Hash;
-use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
-class UpdateEmailController extends Controller {
-
-    public function index() {
-
+class UpdateEmailController extends Controller
+{
+    public function index()
+    {
         if (config('aether.can_change_email')) {
             return view('account.email');
         }
+
         return abort(404);
     }
 
-    public function store(Request $request) {
-
+    public function store(Request $request)
+    {
         if (!(Hash::check($request->get('password'), Auth::user()->password))) {
-            return redirect()->back()->withErrors("You have entered an incorrect password. Please try again.");
+            return redirect()->back()->withErrors('You have entered an incorrect password. Please try again.');
         }
 
         $request->validate([
-            'password' => ['required', 'password'],
-            'email' => ['required', 'password', Rule::unique('users')->ignore(Auth::user()->id)],
+            'password'  => ['required', 'password'],
+            'email'     => ['required', 'password', Rule::unique('users')->ignore(Auth::user()->id)],
             'new_email' => ['required', 'min:6', 'confirmed', 'string'],
         ]);
 
@@ -37,5 +38,4 @@ class UpdateEmailController extends Controller {
 
         return redirect()->back()->with('account-success', 'Your account has been updated successfully.');
     }
-
 }
